6db8ba686b39cdbd5ad90179c619fe586cf4e439
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-08-30 16:21:06 +0700

    add 6.0

--- app/index.php
+++ app/index.php
@@ -8,14 +8,18 @@ echo "</pre>";
 echo "connecting to mysql";
 echo "<pre></pre>";
 
+$servername = getenv('DEV_DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
-$servername = $_ENV['DEV_DB_HOST'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
 
-echo "<pre>";
-print_r($_ENV);
-echo "</pre>";
+// $servername = $_ENV['DEV_DB_HOST'];
+// $username = $_ENV['DB_USER'];
+// $password = $_ENV['DB_PASSWORD'];
+
+// echo "<pre>";
+// print_r($_ENV);
+// echo "</pre>";
 
 // $servername = "127.0.0.1";
 // $username = "proxyuser";
