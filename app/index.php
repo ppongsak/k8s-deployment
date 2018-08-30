c91b4b31f9d7eef380645f83ef5d04c6deda7263
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-08-30 16:06:43 +0700

    test env

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php
-echo "k8-deployment 2.0.0 yaml";
+echo "k8-deployment 3.0.0 yaml";
 
 echo "<pre>";
 echo "======================================================================";
@@ -9,13 +9,13 @@ echo "connecting to mysql";
 echo "<pre></pre>";
 
 
-// $servername = getenv('DEV_DB_HOST');
-// $username = getenv('DB_USER');
-// $password = getenv('DB_PASSWORD');
+$servername = getenv('DEV_DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
-$servername = "127.0.0.1";
-$username = "proxyuser";
-$password = "rebate1234";
+// $servername = "127.0.0.1";
+// $username = "proxyuser";
+// $password = "rebate1234";
 
 echo $servername;
 echo "<pre></pre>";
